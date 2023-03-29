3c85b525c7d5f88338ac08e8d9184d97941525a6
EgoMoose <EgoMoose@users.noreply.github.com>
2023-03-29 16:07:42 -0400

    Add first version

--- src/Modifiers/GravityCamera.lua
+++ src/Modifiers/GravityCamera.lua
@@ -1,3 +1,161 @@
+local transitionRate: number = 0.15
+
+local upCFrame: CFrame = CFrame.new()
+local upVector: Vector3 = upCFrame.YVector
+local twistCFrame: CFrame = CFrame.new()
+
+local spinPart: BasePart = workspace.Terrain
+local prevSpinPart: BasePart = spinPart
+local prevSpinCFrame: CFrame = spinPart.CFrame
+
+--
+
+local function getRotationBetween(u: Vector3, v: Vector3, axis: Vector3): CFrame
+	local dot, uxv = u:Dot(v), u:Cross(v)
+	if dot < -0.99999 then return CFrame.fromAxisAngle(axis, math.pi) end
+	return CFrame.new(0, 0, 0, uxv.x, uxv.y, uxv.z, 1 + dot)
+end
+
+local function calculateUpStep(dt: number, targetUpVector: Vector3)
+	local axis = workspace.CurrentCamera.CFrame.RightVector
+
+	local sphericalArc = getRotationBetween(upVector, targetUpVector, axis)
+	local transitionCF = CFrame.new():Lerp(sphericalArc, transitionRate * math.max(dt * 60, 1))
+
+	upVector = transitionCF * upVector
+	upCFrame = transitionCF * upCFrame
+end
+
+local function twistAngle(cf: CFrame, direction: Vector3): number
+	local axis, theta = cf:ToAxisAngle()
+	local w, v = math.cos(theta/2),  math.sin(theta/2) * axis
+	local proj = v:Dot(direction) * direction
+	local twist = CFrame.new(0, 0, 0, proj.x, proj.y, proj.z, w)
+	local _nAxis, nTheta = twist:ToAxisAngle()
+	return math.sign(v:Dot(direction)) * nTheta
+end
+
+local function calculateSpinStep(_dt: number)
+	local theta = 0
+	
+	if spinPart == prevSpinPart then
+		local rotation = spinPart.CFrame - spinPart.CFrame.Position
+		local prevRotation = prevSpinCFrame - prevSpinCFrame.Position
+
+		local spinAxis = rotation:VectorToObjectSpace(upVector)
+		theta = twistAngle(prevRotation:ToObjectSpace(rotation), spinAxis)
+	end
+
+	twistCFrame = CFrame.fromEulerAnglesYXZ(0, theta, 0)
+
+	prevSpinPart = spinPart
+	prevSpinCFrame = spinPart.CFrame
+end
+
+--
+
 return function(PlayerModule)
-	print("Gravity Camera")
+	------------
+	local cameraUtils = require(PlayerModule.CameraModule.CameraUtils)
+
+	function cameraUtils.getAngleBetweenXZVectors(v1: Vector3, v2: Vector3): number
+		v1 = upCFrame:VectorToObjectSpace(v1)
+		v2 = upCFrame:VectorToObjectSpace(v2)
+	
+		return math.atan2(
+			v2.X*v1.Z - v2.Z*v1.X, 
+			v2.X*v1.X + v2.Z*v1.Z
+		)
+	end
+
+	------------
+	local poppercam = require(PlayerModule.CameraModule.Poppercam)
+	local zoomController = require(PlayerModule.CameraModule.ZoomController)
+
+	function poppercam:Update(renderDt: number, desiredCameraCFrame: CFrame, desiredCameraFocus: CFrame, _cameraController: any)
+		local rotatedFocus = desiredCameraFocus * (desiredCameraCFrame - desiredCameraCFrame.Position)
+		local extrapolation = self.focusExtrapolator:Step(renderDt, rotatedFocus)
+		local zoom = zoomController.Update(renderDt, rotatedFocus, extrapolation)
+		return rotatedFocus*CFrame.new(0, 0, zoom), desiredCameraFocus
+	end	
+
+	------------
+	local baseCamera = require(PlayerModule.CameraModule.BaseCamera)
+
+	local max_y = math.rad(80)
+	local min_y = math.rad(-80)
+
+	function baseCamera:CalculateNewLookCFrameFromArg(suppliedLookVector: Vector3?, rotateInput: Vector2): CFrame
+		local currLookVector: Vector3 = suppliedLookVector or self:GetCameraLookVector()
+		currLookVector = upCFrame:VectorToObjectSpace(currLookVector)
+
+		local currPitchAngle = math.asin(currLookVector.Y)
+		local yTheta = math.clamp(rotateInput.Y, -max_y + currPitchAngle, -min_y + currPitchAngle)
+		local constrainedRotateInput = Vector2.new(rotateInput.X, yTheta)
+		local startCFrame = CFrame.new(Vector3.zero, currLookVector)
+		local newLookCFrame = CFrame.Angles(0, -constrainedRotateInput.X, 0) * startCFrame * CFrame.Angles(-constrainedRotateInput.Y,0,0)
+
+		return newLookCFrame
+	end
+
+	------------
+	local vehicleCameraCore = require(PlayerModule.CameraModule.VehicleCamera.VehicleCameraCore)
+	local setTransform = vehicleCameraCore.setTransform
+
+	function vehicleCameraCore:setTransform(transform: CFrame)
+		transform = upCFrame:ToObjectSpace(transform.Rotation) + transform.Position
+		return setTransform(self, transform)
+	end
+
+	------------
+	local cameraObject = require(PlayerModule.CameraModule)
+	local cameraInput = require(PlayerModule.CameraModule.CameraInput)
+
+	function cameraObject:GetUpVector(prevUpVector: Vector3)
+		return prevUpVector
+	end
+
+	function cameraObject:SetSpinPart(part: BasePart)
+		spinPart = part
+	end
+
+	function cameraObject:SetTransitionRate(rate: number)
+		transitionRate = rate
+	end
+
+	function cameraObject:GetTransitionRate(): number
+		return transitionRate
+	end
+
+	function cameraObject:Update(dt: number)
+		if self.activeCameraController then
+			self.activeCameraController:UpdateMouseBehavior()
+	
+			local newCameraCFrame, newCameraFocus = self.activeCameraController:Update(dt)
+
+			calculateUpStep(dt, self:GetUpVector(upVector))
+			calculateSpinStep(dt)
+
+			-- newCameraFocus = newCameraFocus
+			newCameraCFrame = newCameraFocus * upCFrame * twistCFrame * newCameraFocus:ToObjectSpace(newCameraCFrame)
+	
+			if self.activeOcclusionModule then
+				newCameraCFrame, newCameraFocus = self.activeOcclusionModule:Update(dt, newCameraCFrame, newCameraFocus)
+			end
+	
+			-- Here is where the new CFrame and Focus are set for this render frame
+			local currentCamera = game.Workspace.CurrentCamera :: Camera
+			currentCamera.CFrame = newCameraCFrame
+			currentCamera.Focus = newCameraFocus
+	
+			-- Update to character local transparency as needed based on camera-to-subject distance
+			if self.activeTransparencyController then
+				self.activeTransparencyController:Update(dt)
+			end
+	
+			if cameraInput.getInputEnabled() then
+				cameraInput.resetInputForFrameEnd()
+			end
+		end
+	end
 end
--- test/PlayerScriptsLoader.client.lua
+++ test/PlayerScriptsLoader.client.lua
@@ -2,3 +2,27 @@ local PlayerModule = script.Parent:WaitForChild("PlayerModule")
 
 local playerModuleObject = require(PlayerModule)
 local cameraModuleObject = playerModuleObject:GetCameras()
+
+local upVector = Vector3.new(0, 1, 0)
+
+function cameraModuleObject:GetUpVector()
+	return upVector
+end
+
+game:GetService("RunService").Heartbeat:Connect(function(_dt)
+	local character = game.Players.LocalPlayer.Character
+	local hrp = character and character:FindFirstChild("HumanoidRootPart")
+
+	if hrp then
+		local params = RaycastParams.new()
+		params.FilterType = Enum.RaycastFilterType.Exclude
+		params.FilterDescendantsInstances = {character}
+
+		local result = workspace:Raycast(hrp.Position, hrp.CFrame.YVector * -5, params)
+
+		if result then
+			upVector = result.Normal
+			cameraModuleObject:SetSpinPart(result.Instance)
+		end
+	end
+end)
